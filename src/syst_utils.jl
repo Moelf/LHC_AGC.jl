bc0c3ed4726dbcb6743cab20b8db97bfed6fb642
Jerry Ling <proton@jling.dev>
2022-09-30 12:52:24 -0400

    add missing file

---
+++ src/syst_utils.jl
@@ -0,0 +1,14 @@
+function flat_variation(ones)
+    # 2.5% weight variations
+    return 1 + 0.025, 1 - 0.025
+end
+
+function btag_weight_variation(jet_pt)
+    pt_var = 0.025 * jet_pt / 50 #2.5% per 50 GeV
+    1 + pt_var, 1 - pt_var
+end
+
+function jet_pt_resolution(pt)
+    d = Normal(1, 0.05)
+    rand(d)
+end
